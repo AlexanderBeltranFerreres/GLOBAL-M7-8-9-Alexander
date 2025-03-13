824cc90eacad191e6923dc0d3ebfd01f4aec10d8
AlexanderBeltranFerreres <abeltran3@iesebre.com>
2025-03-13 23:19:30 +0100

    Acabat VideosTest

--- tests/Unit/VideosTest.php
+++ tests/Unit/VideosTest.php
@@ -40,6 +40,28 @@ class VideosTest extends TestCase
         $response->assertSee('Videos');
     }
 
+    public function test_user_with_permissions_can_see_default_videos_page()
+    {
+        $user = User::factory()->create();
+        $user->givePermissionTo('manage videos');
+
+        // Realitzar una petició GET a la pàgina per defecte de vídeos
+        $response = $this->actingAs($user)->get(route('videos.index'));
+
+        // Verificar que l'usuari pot veure la pàgina de vídeos
+        $response->assertStatus(200);
+        $response->assertSee('Videos');
+    }
+
+    public function test_not_logged_users_can_see_default_videos_page()
+    {
+        // Realitzar una petició GET a la pàgina per defecte de vídeos sense estar loguejat
+        $response = $this->get(route('videos.index'));
+
+        // Verificar que la resposta és exitosa i l'usuari no ha d'estar autenticat
+        $response->assertStatus(200);
+        $response->assertSee('Videos');
+    }
 
 
 }
